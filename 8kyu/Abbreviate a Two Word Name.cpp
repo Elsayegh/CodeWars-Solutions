1a94cb86cc584313bd42f89d41fdc350e5b63d2d
Wael Elsayegh <31796128+Elsayegh@users.noreply.github.com>
2019-09-02 14:17:10 +0100

    Create Abbreviate a Two Word Name.cpp

---
+++ 8kyu/Abbreviate a Two Word Name.cpp
@@ -0,0 +1,21 @@
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot seperating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+Patrick Feeney => P.F
+
+
+
+*/
+
+//Solution
+
+std::string abbrevName(std::string name)
+{
+  return {toupper(name[0]), '.' , toupper(name[name.find_last_of(' ') + 1])};
+}
