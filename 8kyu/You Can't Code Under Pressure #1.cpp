661b29439706718508fe94ba59b874dc9d1e8cd7
Wael Elsayegh <31796128+Elsayegh@users.noreply.github.com>
2019-09-02 13:20:31 +0100

    Create You Can't Code Under Pressure #1.cpp

---
+++ 8kyu/You Can't Code Under Pressure #1.cpp
@@ -0,0 +1,13 @@
+/*
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+//Solution
+
+#include <cstdint>
+
+int32_t double_integer(int32_t n)
+{
+  // We need solution fast !!!
+  return n * 2;
+}
