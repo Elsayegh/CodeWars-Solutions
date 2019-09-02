fcdca0d5c54f1e6cbe8595ca9f7b21744f7486e2
Wael Elsayegh <31796128+Elsayegh@users.noreply.github.com>
2019-09-02 13:49:44 +0100

    Create Function 3 - multiplying two numbers.cpp

---
+++ 8kyu/Function 3 - multiplying two numbers.cpp
@@ -0,0 +1,13 @@
+/*
+This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+
+
+*/
+
+//Solution
+
+// Write your multiply function here
+int multiply(int x, int y)
+{
+  return x * y;
+}
