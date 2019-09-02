ed3f3ee6f165f8fabffbd574277fc1fb19c04a32
Wael Elsayegh <31796128+Elsayegh@users.noreply.github.com>
2019-09-02 12:16:15 +0100

    Create Calculate BMI.cpp

---
+++ 8kyu/Calculate BMI.cpp
@@ -0,0 +1,32 @@
+/*
+Write function bmi that calculates body mass index (bmi = weight / height ^ 2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+
+
+
+*/
+
+//Solution
+
+#include<math.h>
+
+std::string bmi(double w, double h) 
+{
+  
+  //TODO: Implement this function
+ 
+   
+   double bmi = w / h / h;
+   
+    if(bmi <= 18.5) return "Underweight";
+  if(bmi <= 25.0) return "Normal";
+  if(bmi <= 30.0) return "Overweight";
+  return "Obese";
+}
