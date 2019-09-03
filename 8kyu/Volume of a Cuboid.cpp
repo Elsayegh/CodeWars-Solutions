982e841ddc0e0c808f3a4ecf6f3b472d3ac1007b
Wael Elsayegh <31796128+Elsayegh@users.noreply.github.com>
2019-09-03 09:31:54 +0100

    Create Volume of a Cuboid.cpp

---
+++ 8kyu/Volume of a Cuboid.cpp
@@ -0,0 +1,11 @@
+/*
+Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid.
+Write a function to help Bob with this calculation.
+*/
+
+//Solution
+
+double getVolumeOfCubiod(double length, double width, double height) {
+  // Your code here...
+  return length * width * height;
+}
