88ebafd3086712635fddfba4c5da68e587a80c19
Wael Elsayegh <31796128+Elsayegh@users.noreply.github.com>
2019-09-05 16:43:14 +0100

    Create Equals without "==".cpp

---
+++ 8kyu/Equals without "==".cpp
@@ -0,0 +1,11 @@
+/*
+Your task is to write a function bool equals(int a, int b) what returns same result as a == b, but you must not use == operator.
+
+To check that, the test case will search = character in your solution. So don't use it even in comments.
+*/
+
+//Solution
+
+bool equals(int a, int b){
+  return a ^ b ? false: true;
+}
