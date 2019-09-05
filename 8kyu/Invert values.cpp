ac1d96ae44a2f4e4f578574dc4009eed46d609c9
Wael Elsayegh <31796128+Elsayegh@users.noreply.github.com>
2019-09-05 13:37:58 +0100

    Create Invert values.cpp

---
+++ 8kyu/Invert values.cpp
@@ -0,0 +1,17 @@
+/*
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+*/
+
+//Solution
+#include <vector>
+
+std::vector<int> invert(std::vector<int> values)
+{
+    for(int i = 0; i < values.size(); i++)
+       values[i] = -values[i];
+       return values;
+}
