d2862100013c0e7d8e1074b33c32dcf737ae08a6
Wael Elsayegh <31796128+Elsayegh@users.noreply.github.com>
2019-09-10 10:35:37 +0100

    Create Number climber.cpp

---
+++ 7kyu/Number climber.cpp
@@ -0,0 +1,27 @@
+/*
+For every positive integer N, there exists a unique sequence starting with 1 and ending with N and such that every number in the sequence is either the double of the preceeding number or the double plus 1.
+
+For example, given N = 13, the sequence is [1, 3, 6, 13], because . . . :
+
+ 3 =  2*1 +1
+ 6 =  2*3
+ 13 = 2*6 +1
+Write a function that returns this sequence given a number N. Try generating the elements of the resulting list in ascending order, i.e., without resorting to a list reversal or prependig the elements to a list.
+*/
+
+
+//Solution
+
+#include <cmath>
+#include <algorithm>
+std::vector<int> climb(int n)
+{
+    std::vector<int> sequence;
+    while(n >= 1)
+    {
+      sequence.push_back(n);
+      n = floor(n / 2);
+    }
+    std::reverse (sequence.begin(), sequence.end());
+    return sequence;
+}
